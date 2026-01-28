134905265f9bf2fb407e820db884a38a5509940c
Jaradrj <jara.derooij@edu.tbz.ch>
2026-01-28 14:41:31 +0100

    fix frontend deployment

--- frontend_react/src/react-app-env.d.ts
+++ frontend_react/src/react-app-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="react-scripts" />
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  MODE?: string;
-  VITE_BASEURL?: string;
-  [key: string]: any;
-}
-
-interface ImportMeta {
-  env: ImportMetaEnv;
-}
